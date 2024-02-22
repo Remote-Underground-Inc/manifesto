d406a7ba30c7b4d58c31d1f51d38242cb99c11b5
Thibault Ollivier <thibaultollr@gmail.com>
2024-02-22 11:22:25 +0100

    eslint & prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,38 @@ const isDev = process.env.APP_ENV === 'development';
 const baseFilename = isDev ? 'main' : 'main.[contenthash]';
 
 module.exports = {
-    entry: [
-        path.resolve(__dirname, 'theme', 'js', 'main.js'),
-        path.resolve(__dirname, 'theme', 'css', 'main.scss'),
-    ],
-    output: {
-        path: path.resolve(__dirname, 'public', 'assets'),
-        filename: `${baseFilename}.js`,
-    },
+  entry: [
+    path.resolve(__dirname, 'theme', 'js', 'main.js'),
+    path.resolve(__dirname, 'theme', 'css', 'main.scss'),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets'),
+    filename: `${baseFilename}.js`,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                        },
-                    },
-                ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
             },
-            {
-                test: /\.(s(a|c)ss)$/,
-                use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
-             }
+          },
         ],
-    },
-
-    plugins: [
-        new WebpackManifestPlugin({ publicPath: '/assets/' }),
-        new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
+  },
+
+  plugins: [
+    new WebpackManifestPlugin({ publicPath: '/assets/' }),
+    new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
+  ],
 };
